4aedbb594dbaab0fe9b343567db0322a325d55a2
1720500851 <1720500851@qq.com>
2021-01-07 17:30:10 +0800

    第一次提交

---
+++ src/com/itheima/Travel.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author TongHu
+ * @date 2021/1/7 - 17:23
+ */
+public class Travel {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
